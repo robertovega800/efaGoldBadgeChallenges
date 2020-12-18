0c908333ac30d506c84143da4a721d0cb2082b35
Roberto Vega <robertovega800@gmail.com>
2020-12-17 23:44:04 -0500

    couple changes

---
+++ KomodoBadge_UnitTests/KomodoBadgeRepoTests.cs
@@ -0,0 +1,23 @@
+﻿using KomodoBadge_Repo;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace KomodoBadge_UnitTests
+{
+    [TestClass]
+    public class KomodoBadgeRepoTests
+    {
+        [TestMethod]
+        public void AddToDictionary_ShouldNotGetNull()
+        {
+            KomodoBadge badge = new KomodoBadge();
+            badge.BadgeID = int.Parse("01");
+            KomodoBadgeRepo repo = new KomodoBadgeRepo();
+
+            repo.AddToDatabase(badge);
+            KomodoBadge badgeFromDatabase = repo.GetBadgeByDictKey(01);
+
+            Assert.IsNotNull(badgeFromDatabase);
+        }
+    }
+}
---
+++ KomodoBadge_UnitTests/KomodoBadgeTests.cs
@@ -0,0 +1,23 @@
+﻿using KomodoBadge_Repo;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace KomodoBadge_UnitTests
+{
+    [TestClass]
+    public class KomodoBadgeTests
+    {
+        [TestMethod]
+        public void SetBadgId_ShouldSetCorrectInt()
+        {
+            KomodoBadge badge = new KomodoBadge();
+
+            badge.BadgeID = int.Parse("01");
+
+            int expected = 1;
+            int actual = badge.BadgeID;
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
